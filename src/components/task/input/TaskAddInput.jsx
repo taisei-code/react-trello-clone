73071404fb2a26924b978d6063bd551163293009
Taisei <taisei96.web@gmail.com>
2023-06-27 13:13:58 +0900

    TaskAddInputコンポーネント作成

--- src/components/task/input/TaskAddInput.jsx
+++ src/components/task/input/TaskAddInput.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
 
 export const TaskAddInput = () => {
-  return 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e)
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="add a task" className='taskAddInput' />
+      </form>
+    </div>
+  )
 }
