393ceedaee092edebfb96299b3f545821904e38d
Taisei <taisei96.web@gmail.com>
2023-06-27 12:51:51 +0900

    微調整

--- src/components/task/TaskCardTitle.jsx
+++ src/components/task/TaskCardTitle.jsx
@@ -23,11 +23,23 @@ export const TaskCardTitle = () => {
     setIsClick(false);
   }
 
+  const handleBlur = () => {
+      setIsClick(false);
+  }
+
   return (
-    <div onClick={handleClick}>
+    <div onClick={handleClick} className='taskCardTitleInputArea'>
       {isClick ? (
         <form onSubmit={handleSubmit} >
-          <input type="text" onChange={handleChange} />
+          <input
+            className='taskCardTitleInput'
+            autoFocus
+            type="text"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={inputCardTitle}
+            maxLength="10"
+          />
         </form>
       ) : (
           <h3>{inputCardTitle}</h3>
